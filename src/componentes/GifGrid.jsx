84b3be854e416bed29a2db21a228fa909015b481
JorgeDevmm <jorge29luis10@gmail.com>
2024-03-18 13:44:23 -0500

    Agregando useEffect, para la obtención del api de los gif

--- src/componentes/GifGrid.jsx
+++ src/componentes/GifGrid.jsx
@@ -1,18 +1,35 @@
 import PropTypes from 'prop-types';
-import getGifs from '../helpers/GetGifs';
-
+import getGifs from '../helpers/getGifs';
+import { useEffect, useState } from 'react';
 
 function GifGrid({ categorie }) {
+  // actualizar estados de la imagenes
+  const [images, setImages] = useState([]);
 
+  // función para obtener las imagenes asicronas de la promesa del getGifs
   // invocamos la función de la API GIF
-  getGifs(categorie);
+  const getImages = async () => {
+    const newImages = await getGifs(categorie);
+    // actualizamos el estado de images con la imagenes obtenidas
+    setImages(newImages);
+  };
+
+  // hook se ejecutara una sola vez, por [] dependencias
+  useEffect(() => {
+    getImages();
+  }, []); /*Se ejecuta una sola vez al inicio */
 
   return (
     <>
       <h3 className='text-4xl text-white'>{categorie}</h3>
-      <div key={categorie} className='text-white font-bold'>
-        {categorie}
-      </div>
+      <ol className='text-white'>
+        {/* { images.map} */}
+        <li>titulo</li>
+        <li>titulo</li>
+        <li>titulo</li>
+        <li>titulo</li>
+        <li>titulo</li>
+      </ol>
     </>
   );
 }
--- src/helpers/GetGifs.js
+++ src/helpers/GetGifs.js
@@ -1,6 +1,6 @@
-const GetGifs = async (categorie) => {
+const getGifs = async (categorie) => {
   // ruta de url
-  const url = `https://api.giphy.com/v1/gifs/search?api_key=jpdRDe8hVf54hZZG8n3HzZbdhhIVMmOb&q=${categorie}&limit=20`;
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=jpdRDe8hVf54hZZG8n3HzZbdhhIVMmOb&q=${categorie}&limit=10`;
 
   // esperamos que se complete la petición url del fetch
   const respuesta = await fetch(url);
@@ -10,14 +10,13 @@ const GetGifs = async (categorie) => {
 
   // iteramos el array data y creamos un nuevo array y vamos creando un nuevo objeto con 3 propiedades
   const gifs = data.map((obj) => ({
-    id: obj.img,
+    id: obj.id,
     title: obj.title,
     url: obj.images.downsized_medium.url,
   }));
 
   console.log(gifs);
-
   return gifs;
 };
 
-export default GetGifs;
+export default getGifs;
